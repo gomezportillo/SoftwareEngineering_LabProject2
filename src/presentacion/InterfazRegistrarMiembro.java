902cac575d933dafb6d0d97c44b9a586e10cd90d
Pedroma Gómez-Portillo <pedroga_tuenti@hotmail.com>
2014-12-29 15:59:38 +0100

    Create InterfazRegistrarMiembro.java

---
+++ src/presentacion/InterfazRegistrarMiembro.java
@@ -0,0 +1,89 @@
+package presentación;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import java.awt.Color;
+
+import javax.swing.JTextPane;
+
+import dominio.GestorMiembros;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class InterfazRegistrarMiembro extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldLogin;
+	private JTextField textFieldPassword;
+	private JTextPane textPane;
+	
+	/**
+	 * Create the frame.
+	 */
+	public InterfazRegistrarMiembro() {
+		setTitle("GoFit - Registrar nuevo miembro");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 420, 285);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Password:");
+		lblLogin.setBounds(6, 81, 69, 16);
+		contentPane.add(lblLogin);
+		
+		JLabel label = new JLabel("Login:");
+		label.setBounds(6, 37, 69, 16);
+		contentPane.add(label);
+		
+		textFieldLogin = new JTextField();
+		textFieldLogin.setBounds(87, 31, 134, 28);
+		contentPane.add(textFieldLogin);
+		textFieldLogin.setColumns(10);
+		
+		textFieldPassword = new JTextField();
+		textFieldPassword.setColumns(10);
+		textFieldPassword.setBounds(87, 75, 134, 28);
+		contentPane.add(textFieldPassword);
+		
+		JButton btnAltaUsuario = new JButton("Alta usuario");
+		btnAltaUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					if(GestorMiembros.nuevoUsuario(textFieldLogin.getText(), textFieldPassword.getText()))
+							textPane.setText("Usuario creado correctamente");
+					else
+							textPane.setText("Usuario ya existente");					
+				} catch (Exception e) {
+					textPane.setText("Error al cargar base de datos");
+					}
+			}
+		});
+		btnAltaUsuario.setBounds(253, 76, 117, 29);
+		contentPane.add(btnAltaUsuario);
+		
+		JLabel label_1 = new JLabel("Estado");
+		label_1.setForeground(Color.RED);
+		label_1.setBounds(6, 126, 61, 16);
+		contentPane.add(label_1);
+		
+		textPane = new JTextPane();
+		textPane.setToolTipText("Panel para mostrar el restultado de la comprobacion de login o las excepciones lanzadas");
+		textPane.setEditable(false);
+		textPane.setBounds(6, 154, 407, 102);
+		contentPane.add(textPane);
+	}
+}
